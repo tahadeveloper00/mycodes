69d89287845ba5d224653c373402999823042c80
Ahmet Taha ERDAL <62606273+tahadeveloper0@users.noreply.github.com>
2020-03-25 15:08:41 +0300

    Create github.cpp
    Girilen sayıyı tersten yazdırma programı

---
+++ github.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+{
+	int sayi;
+	cout << "Tersten yazdirilacak sayiyi giriniz:";
+	cin >> sayi;
+	while (sayi != 0)
+	{
+		cout << sayi % 10;
+		sayi /= 10;
+	}
+
+
+}
+
